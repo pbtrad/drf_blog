58fc9bcdb7b0a239fd34f96889c48d9a284747ce
paul <paulbrowne2@gmail.com>
2022-02-16 21:25:34 +0000

    Added IsAuthenticatedOrReadOnly permissions project wide in settings

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
 
