6f0c32114d374e18e00ffda045098901e8659ef0
paul <paulbrowne2@gmail.com>
2022-02-17 07:18:58 +0000

    Created custom permission view, added api-auth urls

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,6 +1,17 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissionsOrAnonReadOnly
+
+
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
