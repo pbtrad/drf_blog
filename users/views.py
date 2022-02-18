6ee2ec09f1874c3f0cfadb303184885a977d55ae
paul <paulbrowne2@gmail.com>
2022-02-18 05:50:32 +0000

    Changed blog api views to viewset

--- users/views.py
+++ users/views.py
@@ -1,12 +1,37 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import status, generics
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
+from rest_framework import viewsets
+from rest_framework import filters
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from .serializers import CustomUserSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import AllowAny
 
 
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
+
+class PostList(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get_object(self, queryset=None, **kwargs):
+        item = self.kwargs.get('pk')
+        return get_object_or_404(Post, slug=item)
+
+    # Define Custom Queryset
+    def get_queryset(self):
+        return Post.objects.all()
+
+'''
 class CustomUserCreate(generics.ListCreateAPIView):
     permission_classes = [AllowAny]
 
@@ -32,3 +57,4 @@ class BlacklistTokenUpdateView(APIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+'''
